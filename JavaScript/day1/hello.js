7f2efa7aaf27c9b9666ef7c9d96a9d9538a3ca05
20Sangwoo <144245121+20Sangwoo@users.noreply.github.com>
2025-04-15 13:45:13 +0900

    Add files via upload

---
+++ JavaScript/day1/hello.js
@@ -0,0 +1,19 @@
+console.log('Hello World !');
+
+let x = 10;
+let y = 20;
+let sum = x + y;
+console.log(sum);
+
+sum = 0;
+for (i=1; i<=10; i++) {
+    sum += i;
+}
+
+console.log('sum = ' + sum)
+Hello();
+
+
+function Hello() {
+    console.log("Hello");
+}
